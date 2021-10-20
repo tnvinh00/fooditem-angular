ecacea1608d79df0fd675eafe6d19f36b3d4cfc8
tnvinh00 <vinhngoc.trinh@dxc.com>
2021-10-20 10:29:02 +0700

    add function addtoCart, change title - listfood (home)

--- src/app/components/list-food/list-food.component.ts
+++ src/app/components/list-food/list-food.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Food } from 'src/app/food';
+import { Food } from 'src/app/models/food';
 import { FoodService } from 'src/app/food.service';
-
+import { ToastrService } from 'ngx-toastr';
+import { CartService } from 'src/app/cart.service';
+import { CartItem } from 'src/app/models/cartitem';
 
 @Component({
     selector: 'app-list-food',
@@ -11,8 +13,14 @@ import { FoodService } from 'src/app/food.service';
 
 export class ListFoodComponent implements OnInit {
     data: Food[] = []
+    sortedData: Food[] = []
+    searchString;
+    sortby: string;
 
-    constructor(private foodService: FoodService) { }
+    constructor(
+        private foodService: FoodService,
+        private cartService: CartService,
+    ) { }
 
     ngOnInit(): void {
         this.getListFood();
@@ -21,6 +29,28 @@ export class ListFoodComponent implements OnInit {
     getListFood() {
         this.foodService.get().subscribe((res: any) => {
             this.data = res
+            this.sortedData = res
         })
+    }
+
+    searchFood(e) {
+        if (e === '') {
+            this.sortedData = this.data;
+            return;
+        }
+        this.sortedData = this.data.filter(({ title }) => title.toLowerCase().includes(e.toString().toLowerCase()));
+    }
+
+    changeSort(e){
+        switch(e){
+            case 'increase': this.sortedData.sort((a, b) => a.price - b.price); break;
+            case 'decrease': this.sortedData.sort((a, b) => b.price - a.price); break;
+            case 'highrate': this.sortedData.sort((a, b) => a.rate - b.rate); break;
+            case 'lowrate': this.sortedData.sort((a, b) => b.rate - a.rate); break;
+        }
+    }
+
+    addtoCart(index: number) {
+        this.cartService.addtoCart(this.data[index]);
     }
 }
