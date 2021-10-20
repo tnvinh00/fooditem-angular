970f3fdbbcca60cd082d16c034757b6b9f2a37af
tnvinh00 <vinhngoc.trinh@dxc.com>
2021-10-20 10:24:46 +0700

    add btn cart to top bar

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/cart.service';
 
 @Component({
-  selector: 'app-top-bar',
-  templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.css']
+    selector: 'app-top-bar',
+    templateUrl: './top-bar.component.html',
+    styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent implements OnInit {
+    numberofCartItem: number;
+    searchString;
+    
+    constructor(
+        private cartService: CartService,
+    ) {
+        this.numberofCartItem = cartService.getNuberofCart();
+    }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        // this.numberofCartItem = this.cartService.getNuberofCart();
+    }
 
 }
